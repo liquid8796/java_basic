b06ce7c69d6ae516ab8e3c1049e44209e4c598c7
liquid8796 <hanam.tranle.5@gmail.com>
2022-03-03 16:07:56 +0700

    16:07 03/03/2022

--- src/JavaCore/Collections/ComparatorExample.java
+++ src/JavaCore/Collections/ComparatorExample.java
@@ -1,24 +1,31 @@
 package JavaCore.Collections;
 
-import JavaCore.Java8.Student;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ComparatorExample {
+    public static List<Student> filterStudentsByAddress(List<Student> classroom, String addr){
+        List<Student> result = new ArrayList<>();
+        for(Student student : classroom){
+            if(addr.equals(student.getAddress())){
+                result.add(student);
+            }
+        }
+        return result;
+    }
     public static void main(String[] args){
         // create list students
         List<Student> listStudents = new ArrayList<Student>();
         // add students to list
         listStudents.add(new Student(1, "Vinh", 19, "Hanoi"));
-        listStudents.add(new Student(2, "Hoa", 24, "Hanoi"));
-        listStudents.add(new Student(3, "Phu", 20, "Hanoi"));
-        listStudents.add(new Student(4, "Quy", 22, "Hanoi"));
+        listStudents.add(new Student(2, "Hoa", 24, "HCM"));
+        listStudents.add(new Student(3, "Phu", 20, "HCM"));
+        listStudents.add(new Student(4, "Quy", 22, "Da Nang"));
 
         // sort list student by it's name ASC
-        System.out.println("sort list student by it's name ASC: ");
+        System.err.println("sort list student by it's name ASC: ");
         Collections.sort(listStudents, new Comparator<Student>(){
             @Override
             public int compare(Student o1, Student o2) {
@@ -27,10 +34,10 @@ public class ComparatorExample {
         });
         // show list students
         for (Student student : listStudents) {
-            System.err.println(student.toString());
+            System.out.println(student.toString());
         }
         // sort list student by age ASC
-        System.out.println("sort list student by it's age ASC: ");
+        System.err.println("sort list student by it's age ASC: ");
         Collections.sort(listStudents, new Comparator<Student>(){
             @Override
             public int compare(Student o1, Student o2) {
@@ -39,6 +46,13 @@ public class ComparatorExample {
         });
         // show list students
         for (Student student : listStudents) {
+            System.out.println(student.toString());
+        }
+        // filter student by address
+        System.err.println("filter student by address: ");
+        List<Student> filter1 = filterStudentsByAddress(listStudents, "HCM");
+        // show list students
+        for (Student student : filter1) {
             System.out.println(student.toString());
         }
     }
