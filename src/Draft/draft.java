a1bd987d2dba3a7c9b49379b4866126c832f237d
liquid8796 <hanam.tranle.5@gmail.com>
2022-06-05 10:20:30 +0700

    10:19 05/06/2022

--- src/Draft/draft.java
+++ src/Draft/draft.java
@@ -1,33 +1,26 @@
 package Draft;
 
+import JavaCore.Java8.chap5.Trader;
+import JavaCore.Java8.chap5.Transaction;
 import JavaCore.Java8.chap6.Dish;
+import JavaCore.Java8.chap6.ToListCollector;
 
 import java.util.*;
+import java.util.function.IntSupplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static JavaCore.Java8.chap6.Dish.menu;
 import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
 
 
 public class draft {
 
-    public static void printList(int[] list){
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i]+" ");
-        }
-        System.out.println("");
-    }
-
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-        List<Integer> collect = Arrays.stream(nums)
-                .map(x -> x * x)
-                .boxed()
-                .collect(toList());
-        System.out.println(collect);
+
     }
 
 }
